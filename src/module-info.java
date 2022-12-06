17391b3150a6ba9c9981fa12259f6faffb2a86ed
Amiyah12 <37642477+Amiyah12@users.noreply.github.com>
2022-12-06 08:19:03 -0600

    Chapter 2

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,14 @@
-module Calculateavereage {
+module QuartzHuntington 
+
+import java.util.*;
+//Quarts ToGallons.java public class Quarts ToGallons
+public static void main(String[] args)
+{
+l/one quart to hold
+int quarts=18;
+/Icalculate number of "gallons" int gallons=quarts/ONE_GALLON;
+int quart=quarts%ONE_GALLON;
+System.out.printin("A job that needs "+quarts +" quarts requires "+gallons +" gallons plus "+quart+" quarts");
+Y/lend of the class QuartsToGallons
+
 }
